02501e55b14f94dc36e7830714c5b8070176f8c5
elinteligenetemack <Xternalprogmg@XTS-MCFLORES.tasa.com.pe>
2015-10-12 12:59:25 -0500

    Plantilla basica php gratis
    Plantilla basica php gratis

---
+++ footer.php
@@ -0,0 +1,7 @@
+	<footer>
+		<div class="wrapp">
+			<p>Paginas Web - flores</p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>PAGINAS WEB FLORES</title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="index.php"><img src="img/logo.png" alt="Max Flores"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="blog.php">Blog</a></li>
+					<li><a href="acerca_de.php">Acerca de</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
